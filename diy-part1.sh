b8a2f6982223407b9a99477fbc4b781b049981bf
fadediris <33453792+fadediris@users.noreply.github.com>
2021-03-27 00:41:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,9 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+git clone https://github.com/lisaac/luci-lib-docker.git package/DIYss/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman.git package/DIYss/luci-app-dockerman
